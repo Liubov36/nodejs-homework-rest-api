3da334b1cfbec9066c13b53fa0a838ecc55eb788
liuba36 <inkkli24@gmail.com>
2022-12-17 15:48:40 +0100

    task done

---
+++ controllers/contacts/add.js
@@ -0,0 +1,32 @@
+const contactsOperations = require('../../models/contacts');
+
+const Joi = require('joi');
+
+const contactsSchema = Joi.object({
+  name: Joi.string().trim().required(),
+  email: Joi.string().email().trim().required(),
+  phone: Joi.string().required(),
+});
+
+const add = async (req, res, next) => {
+  try {
+    const { error } = contactsSchema.validate(req.body);
+    if (error) {
+      error.status = 400;
+      error.message = `missing required field`;
+      throw error;
+    }
+    const result = await contactsOperations.addContact(req.body);
+    res.status(201).json({
+      status: 'success',
+      code: 201,
+      data: {
+        result,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = add;

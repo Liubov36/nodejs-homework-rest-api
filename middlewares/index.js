991cf780759e99bb3e0eb965a983cd45f2f20bf6
liuba36 <inkkli24@gmail.com>
2022-12-30 21:00:45 +0100

    finish5

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const isValidId = require('./isValidId');
 const validationBody = require('./validationBody');
 const auth = require('./auth');
+const upload = require('./upload');
 
 module.exports = {
     isValidId,
     validationBody,
     auth,
+    upload,
 };
